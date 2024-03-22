471f339df6c80d2e398daa7ee8a211073a9b6887
lsherman98 <122409681+lsherman98@users.noreply.github.com>
2024-03-22 12:19:01 -0400

    done

--- app.js
+++ app.js
@@ -5,9 +5,38 @@ const app = express()
 
 app.use(countAllRequests())
 
+const axios = require('axios')
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    axios
+        .get(`http://localhost:${PORT}/middle-tier`)
+        .then(() => axios.get(`http://localhost:${PORT}/middle-tier`))
+        .then(result => {
+            res.send(result.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).send()
+        })
 })
+
+app.get('/middle-tier', (req, res) => {
+    axios
+        .get(`http://localhost:${PORT}/backend`)
+        .then(() => axios.get(`http://localhost:${PORT}/backend`))
+        .then(result => {
+            res.send(result.data)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).send()
+        })
+})
+
+app.get('/backend', (req, res) => {
+    res.send('Hello from the backend')
+})
+
 
 app.get('/date', (req, res) => {
     res.json({ today: new Date()})
