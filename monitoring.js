869e4bfa17af40a4e14ba361a5d57501ad2353cc
lsherman98 <122409681+lsherman98@users.noreply.github.com>
2024-03-22 12:02:28 -0400

    added prometheus

---
+++ monitoring.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const { MeterProvider, Meter } = require('@opentelemetry/metrics')
+const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus')
+
+const prometheusPort = PrometheusExporter.DEFAULT_OPTIONS.port
+const PrometheusEndpoint = PrometheusExporter.DEFAULT_OPTIONS.endpoint
+
+const exporter = new PrometheusExporter(
+    {
+        startServer: true,
+    },
+    () => {
+        console.log(`prometheus scrape endpoint: http://localhost:${prometheusPort}${PrometheusEndpoint}`)
+    }
+)
+
+const meter = new MeterProvider({
+    exporter,
+    interval: 1000,
+}).getMeter('your-meter-name')
+
+const requestCount = meter.createCounter("requests", {
+    description: "Count all incoming requests"
+})
+
+const boundInstruments = new Map()
+
+module.exports.countAllRequests = () => {
+    return (req, res, next) => {
+        if (!boundInstruments.has(req.path)) {
+            const labels = { route: req.path }
+            const boundCounter = requestCount.bind(labels)
+            boundInstruments.set(req.path, boundCounter)
+        }
+
+        boundInstruments.get(req.path).add(1)
+        next()
+    }
+}
